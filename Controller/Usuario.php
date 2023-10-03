8feda1a594a79744282050a5ba02323a20f8efc6
Davi Bos <dboscardindossantos@gmail.com>
2023-10-03 20:38:39 -0300

    hash, eu acho

--- Controller/Usuario.php
+++ Controller/Usuario.php
@@ -6,11 +6,20 @@ include_once '../Model/Cargo.class.php';
 
 // Cadastrar no banco
 if($acao='cadastrar'){
+
+    $texto = $_GET['senha'];
+
+    $salt =  'yuyukyug';
+
+    $senha = md5( $texto.  $salt) . 'Eu sou hacker!';   
+
+    $md5 = hash('sha256', $texto);
+
     $usuario=new Usuario();
     $cargo=new Cargo();
     $usuario->setNome($_POST['nome']);
     $usuario->setEmail($_POST['email']);
-    $usuario->serSenha($_POST['senha']);
+    $usuario->setSenha($md5);
     $usuario->setDatanasc($_POST['Datanasc']);
     $usuario->setCelular($_POST['Celular']);
     $usuario->setDatacad(date('Y/m/d'));
